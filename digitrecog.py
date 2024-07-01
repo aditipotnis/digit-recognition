1e390ecbca0cc28e24c56b3eafc6dc4ab336acc1
aditipotnis <165708174+aditipotnis@users.noreply.github.com>
2024-07-01 19:05:27 +0530

    batchnormalization, more pooling for accuracy

--- digitrecog.py
+++ digitrecog.py
@@ -1,7 +1,7 @@
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten, BatchNormalization, Dropout, Conv2D, MaxPooling2D
 from keras import backend as K
 
 #parameters
@@ -25,13 +25,22 @@ x_test /= 255
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+model.add(BatchNormalization())
 model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
+
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
 
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
