81cd067b87b03b462de122c6cc868aafb21a8b8c
aditipotnis <165708174+aditipotnis@users.noreply.github.com>
2024-07-01 18:05:02 +0530

    changed gui little

--- guidigit_recog.py
+++ guidigit_recog.py
@@ -27,25 +27,31 @@ def predict_digit(img):
 class App(tk.Tk):
 
 	def __init__(self):
-        	tk.Tk.__init__(self)
-        	self.x = self.y = 0
+		super().__init__()
+		self.x = self.y = 0
+		
+		#Configure window
+		self.title("Regonize Digits(0-9)")
+		self.geometry("500x400")
+		self.configure(bg='lightblue')
 
         	# Creating elements
-        	self.canvas = tk.Canvas(self, width=200, height=200, bg = "white", cursor="cross")
-        	self.label = tk.Label(self, text="Analyzing..", font=("Helvetica", 48))
-        	self.classify_btn = tk.Button(self, text = "Searched", command = self.classify_handwriting) 
-        	self.button_clear = tk.Button(self, text = "Dlt", command = self.clear_all)
+		self.canvas = tk.Canvas(self, width=300, height=300, bg = "white", cursor="cross", bd=5, relief="sunken")
+		self.label = tk.Label(self, text="draw a digit", font=("Helvetica", 48), bg='lightblue')
+		self.classify_btn = tk.Button(self, text = "Recognized!", command = self.classify_handwriting) 
+		self.button_clear = tk.Button(self, text = "clear", command = self.clear_all, bg='red', fg='white', font=("Helvetica", 14))
         
 		# Grid structure
-        	self.canvas.grid(row=0, column=0, pady=2, sticky=tk.W, )
-        	self.label.grid(row=0, column=1,pady=2, padx=2)
-        	self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
-        	self.button_clear.grid(row=1, column=0, pady=2)
-        
-        	self.canvas.bind("<B1-Motion>", self.draw_lines)
+		self.canvas.grid(row=0, column=0, padx=20, pady=20, columnspan=2)
+		self.label.grid(row=1, column=0, pady=10, padx=20)
+		self.classify_btn.grid(row=2, column=1, pady=10, padx=20)
+		self.button_clear.grid(row=2, column=0, pady=10, padx=20) 
+        	
+		self.canvas.bind("<B1-Motion>", self.draw_lines)
 
 	def clear_all(self):
 		self.canvas.delete("all")
+		self.label.configure(text="Draw a digit")
 
 	def classify_handwriting(self):
 		x = self.winfo_rootx() + self.canvas.winfo_x()
@@ -54,13 +60,14 @@ class App(tk.Tk):
 		y1 = y + self.canvas.winfo_height()
 		im = ImageGrab.grab().crop((x, y, x1, y1))
 		digit, acc = predict_digit(im)
-		self.label.configure(text= str(digit) + ', ' + str(int(acc * 100)) + '%')
+		self.label.configure(text=f'{digit}, {int(acc * 100)}%')
 
 	def draw_lines(self, event):
 		self.x = event.x
 		self.y = event.y
 		r = 8
 		self.canvas.create_oval(self.x - r, self.y - r, self.x + r, self.y + r, fill='black')
+		self.canvas.create_line(self.x, self.y, event.x, event.y, width=20, fill='black', capstyle=tk.ROUND, smooth=tk.TRUE)
 
 app = App()
 tk.mainloop()
